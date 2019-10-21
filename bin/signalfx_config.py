6fb49d3b6769063281de2eea09be0e7cc0bed114
Mark Stumpf <mstumpf@signalfx.com>
2019-10-21 17:08:27 +0200

    Linting & formatting issues

--- bin/signalfx_config.py
+++ bin/signalfx_config.py
@@ -1,74 +1,77 @@
-import splunk.admin as admin
-import splunk.entity as en
-
-'''
+"""
 Description:  This skeleton python script handles the parameters in the configuration page.
 
-      handleList method: lists configurable parameters in the configuration page
-      corresponds to handleractions = list in restmap.conf
+  handleList method: lists configurable parameters in the configuration page
+  corresponds to handleractions = list in restmap.conf
 
-      handleEdit method: controls the parameters and saves the values 
-      corresponds to handleractions = edit in restmap.conf
+  handleEdit method: controls the parameters and saves the values 
+  corresponds to handleractions = edit in restmap.conf
 
-'''
+"""
+import splunk.entity as en
+import splunk.admin as admin
+
 
 class ConfigApp(admin.MConfigHandler):
-  '''
-  Set up supported arguments
-  '''
-  def setup(self):
-    if self.requestedAction == admin.ACTION_EDIT:
-      for arg in ['access_token', 'realm', 'ingest_url']:
-        self.supportedArgs.addOptArg(arg)
-        
-  '''
-  Read the initial values of the parameters from the custom file
-      sfx.conf, and write them to the setup page. 
+    """
+    Set up supported arguments
+    """
 
-  If the app has never been set up,
-      uses .../signalfx-forwarder/default/sfx.conf. 
+    def setup(self):
+        if self.requestedAction == admin.ACTION_EDIT:
+            for arg in ["access_token", "realm", "ingest_url"]:
+                self.supportedArgs.addOptArg(arg)
 
-  If app has been set up, looks at 
-      .../local/sfx.conf first, then looks at 
-  .../default/sfx.conf only if there is no value for a field in
-      .../local/sfx.conf
+    def handleList(self, confInfo):
+        """
+        Read the initial values of the parameters from the custom file
+        sfx.conf, and write them to the setup page. 
 
-  For boolean fields, may need to switch the true/false setting.
-  For text fields, if the conf file says None, set to the empty string.
-  '''
+        If the app has never been set up,
+        uses .../signalfx-forwarder/default/sfx.conf. 
 
-  def handleList(self, confInfo):
-    confDict = self.readConf("sfx.conf")
-    if None != confDict:
-      for stanza, settings in confDict.items():
-        for key, val in settings.items():
-          if key in ['access_token'] and val in [None, '']:
-            val = ''
-          if key in ['ingest_url'] and val in [None, '']:
-            val = ''
-          if key in ['realm'] and val in [None, '']:
-            val = ''
+        If app has been set up, looks at 
+        .../local/sfx.conf first, then looks at 
+        .../default/sfx.conf only if there is no value for a field in
+        .../local/sfx.conf
 
-          confInfo[stanza].append(key, val)
-          
-  '''
-  After user clicks Save on setup page, take updated parameters,
-  normalize them, and save them somewhere
-  '''
-  def handleEdit(self, confInfo):
-    ingest_url = self.callerArgs.data['ingest_url'][0]
-    ## if ingest url is not set, check realm
-    if ingest_url in [None, '']:
-      realm = self.callerArgs.data['realm'][0]
-      ## if realm is not set, default to us0
-      if realm in [None, '']:
-        realm = 'us0'
-      self.callerArgs.data['ingest_url'][0] = 'https://ingest.%s.signalfx.com' % realm.lower()
+        For boolean fields, may need to switch the true/false setting.
+        For text fields, if the conf file says None, set to the empty string.
+        """
+        confDict = self.readConf("sfx.conf")
+        if confDict is not None:
+            for stanza, settings in confDict.items():
+                for key, val in settings.items():
+                    if key in ["access_token"] and val in [None, ""]:
+                        val = ""
+                    if key in ["ingest_url"] and val in [None, ""]:
+                        val = ""
+                    if key in ["realm"] and val in [None, ""]:
+                        val = ""
 
-    # Since we are using a conf file to store parameters, 
-    # write them to the [SignalFxConfig] stanza
-    # in splunk-forwarder/local/sfx.conf  
-    self.writeConf('sfx', 'SignalFxConfig', self.callerArgs.data)
-      
+                    confInfo[stanza].append(key, val)
+
+    def handleEdit(self, confInfo):
+        """
+        After user clicks Save on setup page, take updated parameters,
+        normalize them, and save them somewhere
+        """
+        ingest_url = self.callerArgs.data["ingest_url"][0]
+        ## if ingest url is not set, check realm
+        if not ingest_url:
+            realm = self.callerArgs.data["realm"][0]
+            ## if realm is not set, default to us0
+            if not realm:
+                realm = "us0"
+            self.callerArgs.data["ingest_url"][0] = (
+                "https://ingest.%s.signalfx.com" % realm.lower()
+            )
+
+        # Since we are using a conf file to store parameters,
+        # write them to the [SignalFxConfig] stanza
+        # in splunk-forwarder/local/sfx.conf
+        self.writeConf("sfx", "SignalFxConfig", self.callerArgs.data)
+
+
 # initialize the handler
 admin.init(ConfigApp, admin.CONTEXT_NONE)
--- bin/tosfx.py
+++ bin/tosfx.py
@@ -20,35 +20,34 @@ def populatePayload(metric_type, metric_list, payload):
     expanded = []
     for metric, value in metric_list:
         expanded.append(dict([("metric",metric), ("value",value)]))
-        if timestamp
-        cl
+        if timestamp:
             expanded[-1]["timestamp"] = timestamp
-        expanded[-1]["dimensions"] = dimensions
-    if (metric_type in payload):
+    if metric_type in payload:
         # Append in reverse order because SignalFx expects data points oldest to latest
         payload[metric_type] = expanded + payload[metric_type]
     else:
         payload[metric_type] = expanded
 
+
 configs = ConfigParser.ConfigParser(allow_no_value=True)
-localConfig = os.path.abspath(os.path.join(os.getcwd(),"..","local","sfx.conf"))
-defaultConfig = os.path.abspath(os.path.join(os.getcwd(),"..","default","sfx.conf"))
+localConfig = os.path.abspath(os.path.join(os.getcwd(), "..", "local", "sfx.conf"))
+defaultConfig = os.path.abspath(os.path.join(os.getcwd(), "..", "default", "sfx.conf"))
 
 # Read the default section then local so local will take precedence
 configs.read(defaultConfig)
 configs.read(localConfig)
 
 if configs.get("SignalFxConfig", "ingest_url"):
-    ingest_url = configs.get("SignalFxConfig","ingest_url"):
+    ingest_url = configs.get("SignalFxConfig", "ingest_url")
 
 if configs.get("SignalFxConfig", "access_token"):
-    token = configs.get("SignalFxConfig","access_token")
+    token = configs.get("SignalFxConfig", "access_token")
 
 for arg in sys.argv[:]:
     if arg.startswith("dryrun="):
-        dryRun = arg[-1] in ["T","t","True","true"]
+        dryRun = arg[-1] in ["T", "t", "True", "true"]
     elif arg.startswith("debug="):
-        debug = arg[-1] in ["T","t","True","true"]
+        debug = arg[-1] in ["T", "t", "True", "true"]
 
 
 results, dummy, settings = inter.getOrganizedResults()
@@ -62,35 +61,45 @@ for result in results:
     dimensions = dict()
     timestamp = None
     for key, value in result.iteritems():
-        if (value != ""):
+        if value != "":
             if key.startswith("gauge_"):
                 try:
                     value = int(value)
                 except:
                     value = float(value)
-                gauge.append((key[key.find("gauge_")+6:], value))
+                gauge.append((key[key.find("gauge_") + 6 :], value))
             elif key.startswith("counter_"):
-                counter.append((key[len("counter_"):], int(value)))
+                counter.append((key[len("counter_") :], int(value)))
             elif key.startswith("cumulative_counter_"):
-                cumulative_counter.append((key[len("cumulative_counter_"):],int(value)))
+                cumulative_counter.append(
+                    (key[len("cumulative_counter_") :], int(value))
+                )
             elif key == "_time":
-                timestamp = int(float(value)*1000)
-            elif not key.startswith("_") and key!="punct" and not key.startswith("date_"):
-                if (value[0] != "_" and len(value)<256):
-                    dimensions[key.replace(".","_")] = value
+                timestamp = int(float(value) * 1000)
+            elif (
+                not key.startswith("_")
+                and key != "punct"
+                and not key.startswith("date_")
+            ):
+                if value[0] != "_" and len(value) < 256:
+                    dimensions[key.replace(".", "_")] = value
     if debug:
         result["token"] = token
         result["endpoint"] = ingest_url
-    if len(gauge)>0 or len(counter)>0 or len(cumulative_counter)>0:
-        if len(gauge)>0:
+    if len(gauge) > 0 or len(counter) > 0 or len(cumulative_counter) > 0:
+        if len(gauge) > 0:
             populatePayload("gauge", gauge, payload)
-        if len(counter)>0:
+        if len(counter) > 0:
             populatePayload("counter", counter, payload)
-        if len(cumulative_counter)>0:
+        if len(cumulative_counter) > 0:
             populatePayload("cumulative_counter", cumulative_counter, payload)
     outbuffer.append(result)
 if not dryRun:
-    r = requests.post(ingest_url, headers={"X-SF-TOKEN":token,"Content-Type":"application/json"}, data=json.dumps(payload))
+    r = requests.post(
+        ingest_url,
+        headers={"X-SF-TOKEN": token, "Content-Type": "application/json"},
+        data=json.dumps(payload),
+    )
     for result in outbuffer:
         result["status"] = r.status_code
 
