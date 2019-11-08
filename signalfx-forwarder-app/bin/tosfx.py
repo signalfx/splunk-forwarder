da4f0eeee5bec9c08d81ce7359af6749cbada6ab
Ben Keith <bkeith@signalfx.com>
2019-11-08 08:20:22 -0500

    Further code cleanup with pylint passing

--- signalfx-forwarder-app/bin/tosfx.py
+++ signalfx-forwarder-app/bin/tosfx.py
@@ -1,21 +1,19 @@
-import sys
-import splunk.Intersplunk as inter
-import splunk.entity as entity
-import csv
+import configparser
 import json
-import os
-import requests
-import argparse
 import logging
-import ConfigParser
-
+import os
+import sys
 from collections import OrderedDict
+
+import requests
+
+import splunk.Intersplunk as inter  # pylint:disable=import-error
 
 logger = logging.getLogger("SignalFxForwarder")
 logger.setLevel(logging.DEBUG)
 
 
-def populatePayload(metric_type, metric_list, payload, timestamp, dims):
+def populate_payload(metric_type, metric_list, payload, timestamp, dims):
     expanded = []
     for metric, value in metric_list:
         expanded.append(dict([("metric", metric), ("value", value)]))
@@ -31,19 +29,17 @@ def populatePayload(metric_type, metric_list, payload, timestamp, dims):
 
 def main():
     token = ""
-    dryRun = False
+    dry_run = False
     debug = False
     ingest_url = "https://ingest.us0.signalfx.com"
     dp_endpoint = "/v2/datapoint"
-    configs = ConfigParser.ConfigParser(allow_no_value=True)
-    localConfig = os.path.abspath(os.path.join(os.getcwd(), "..", "local", "sfx.conf"))
-    defaultConfig = os.path.abspath(
-        os.path.join(os.getcwd(), "..", "default", "sfx.conf")
-    )
+    configs = configparser.ConfigParser(allow_no_value=True)
+    local_config = os.path.abspath(os.path.join(os.getcwd(), "..", "local", "sfx.conf"))
+    default_config = os.path.abspath(os.path.join(os.getcwd(), "..", "default", "sfx.conf"))
 
     # Read the default section then local so local will take precedence
-    configs.read(defaultConfig)
-    configs.read(localConfig)
+    configs.read(default_config)
+    configs.read(local_config)
 
     if configs.get("setupentity", "ingest_url"):
         ingest_url = configs.get("setupentity", "ingest_url")
@@ -53,74 +49,74 @@ def main():
 
     for arg in sys.argv[:]:
         if arg.startswith("dryrun="):
-            dryRun = arg[-1] in ["T", "t"]
+            dry_run = arg[-1] in ["T", "t"]
         elif arg.startswith("debug="):
             debug = arg[-1] in ["T", "t"]
 
     results, _, _ = inter.getOrganizedResults()
-    logger.debug("signalfx-forwarder-app found %d results in search" % len(results))
+    logger.debug("signalfx-forwarder-app found %d results in search", len(results))
     outbuffer = []
 
     payload = OrderedDict()
     for result in results:
-        gauges = []
-        counters = []
-        cumulative_counters = []
-        dimensions = dict()
-        timestamp = None
-        for key, value in result.iteritems():
-            if value != "":
-                if key.startswith("gauge_"):
-                    try:
-                        value = int(value)
-                    except:
-                        value = float(value)
-                    gauges.append((key[len("gauge_") :], value))
-                elif key.startswith("counter_"):
-                    counters.append((key[len("counter_") :], int(value)))
-                elif key.startswith("cumulative_counter_"):
-                    cumulative_counters.append(
-                        (key[len("cumulative_counter_") :], int(value))
-                    )
-                elif key == "_time":
-                    timestamp = int(float(value) * 1000)
-                elif (
-                    not key.startswith("_")
-                    and key != "punct"
-                    and not key.startswith("date_")
-                ):
-                    if value[0] != "_" and len(value) < 256:
-                        dimensions[key.replace(".", "_")] = value
+        add_result_to_payload(result=result, payload=payload)
 
         if debug:
             result["token"] = token
             result["endpoint"] = ingest_url + dp_endpoint
 
-        if len(gauges) > 0 or len(counters) > 0 or len(cumulative_counters) > 0:
-            if len(gauges) > 0:
-                populatePayload("gauge", gauges, payload, timestamp, dimensions)
-            if len(counters) > 0:
-                populatePayload("counter", counters, payload, timestamp, dimensions)
-            if len(cumulative_counters) > 0:
-                populatePayload("cumulative_counter", cumulative_counters, payload, timestamp, dimensions)
-
         outbuffer.append(result)
 
-    if not dryRun:
-        target = ingest_url + dp_endpoint
-        r = requests.post(
-            target,
-            headers={"X-SF-TOKEN": token, "Content-Type": "application/json"},
-            data=json.dumps(payload),
-        )
+    if not dry_run:
+        resp = send_payload(payload=payload, ingest_url=ingest_url, dp_endpoint=dp_endpoint, token=token)
         for result in outbuffer:
-            result["status"] = r.status_code
+            result["status"] = resp.status_code
 
     inter.outputResults(outbuffer)
 
 
+def add_result_to_payload(result, payload):
+    gauges = []
+    counters = []
+    cumulative_counters = []
+    dimensions = dict()
+    timestamp = None
+    for key, value in result.iteritems():
+        if value != "":
+            if key.startswith("gauge_"):
+                try:
+                    value = int(value)
+                except ValueError:
+                    value = float(value)
+                gauges.append((key[len("gauge_") :], value))
+            elif key.startswith("counter_"):
+                counters.append((key[len("counter_") :], int(value)))
+            elif key.startswith("cumulative_counter_"):
+                cumulative_counters.append((key[len("cumulative_counter_") :], int(value)))
+            elif key == "_time":
+                timestamp = int(float(value) * 1000)
+            elif not key.startswith("_") and key != "punct" and not key.startswith("date_"):
+                if value[0] != "_" and len(value) < 256:
+                    dimensions[key.replace(".", "_")] = value
+
+    if gauges:
+        populate_payload("gauge", gauges, payload, timestamp, dimensions)
+    if counters:
+        populate_payload("counter", counters, payload, timestamp, dimensions)
+    if cumulative_counters:
+        populate_payload("cumulative_counter", cumulative_counters, payload, timestamp, dimensions)
+
+
+def send_payload(payload, ingest_url, dp_endpoint, token):
+    target = ingest_url + dp_endpoint
+    resp = requests.post(
+        target, headers={"X-SF-TOKEN": token, "Content-Type": "application/json"}, data=json.dumps(payload)
+    )
+    return resp
+
+
 try:
     main()
-except Exception as e:
+except Exception as e:  # pylint:disable=broad-except
     logger.exception("Unhanded top-level exception")
     inter.generateErrorResults("Exception! %s (See python.log)" % (e,))
