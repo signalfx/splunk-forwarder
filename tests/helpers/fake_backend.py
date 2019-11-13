ac6b9a600b4f035786c9a3d3f5f238412054f4f9
Ben Keith <bkeith@signalfx.com>
2019-11-13 13:25:25 +0000

    Get tests passing

--- tests/helpers/fake_backend.py
+++ tests/helpers/fake_backend.py
@@ -2,21 +2,10 @@ import asyncio
 import gzip
 import json
 import socket
-import sys
 import threading
-from collections import OrderedDict, defaultdict
 from contextlib import contextmanager
-from queue import Queue
 
-from google.protobuf import json_format
 from sanic import Sanic, response
-from signalfx.generated_protocol_buffers import signal_fx_protocol_buffers_pb2 as sf_pbuf
-
-# This module collects metrics from the agent and can echo them back out for
-# making assertions on the collected metrics.
-from tests.helpers.formatting import get_metric_type
-
-STOP = type("STOP", (), {})
 
 
 def bind_tcp_socket(host="127.0.0.1", port=0):
@@ -30,7 +19,7 @@ def bind_tcp_socket(host="127.0.0.1", port=0):
 # Fake the /v2/datapoint endpoint and just stick all of the metrics in a
 # list
 # pylint: disable=unused-variable
-def _make_fake_ingest(datapoint_queue, events, spans):
+def _make_fake_ingest(datapoints):
     app = Sanic()
 
     @app.middleware("request")
@@ -43,95 +32,20 @@ def _make_fake_ingest(datapoint_queue, events, spans):
     async def handle_datapoints(request):
         is_json = "application/json" in request.headers.get("content-type")
 
-        dp_upload = sf_pbuf.DataPointUploadMessage()
-        if is_json:
-            json_format.Parse(request.body, dp_upload)
-        else:
-            dp_upload.ParseFromString(request.body)
-        datapoint_queue.put(dp_upload)
+        if not is_json:
+            return response.text("Bad Content Type", status=400)
+
+        dp_map = json.loads(request.body)
+
+        out = []
+        for typ, dps in dp_map.items():
+            for dp in dps:
+                dp["type"] = typ
+                out.append(dp)
+
+        datapoints.extend(out)
+
         return response.json("OK")
-
-    @app.post("/v2/event")
-    async def handle_event(request):
-        is_json = "application/json" in request.headers.get("content-type")
-
-        event_upload = sf_pbuf.EventUploadMessage()
-        if is_json:
-            json_format.Parse(request.body, event_upload)
-        else:
-            event_upload.ParseFromString(request.body)
-        events.extend(event_upload.events)  # pylint: disable=no-member
-        return response.json("OK")
-
-    @app.post("/v1/trace")
-    async def handle_trace(request):
-        spans.extend(request.json)
-        return response.json("OK")
-
-    return app
-
-
-# Fake the dimension PUT method to capture dimension property/tag updates.
-# pylint: disable=unused-variable
-def _make_fake_api(dims):
-    app = Sanic()
-
-    @app.get("/v2/dimension/<key>/<value>")
-    async def get_dim(_, key, value):
-        dim = dims.get(key, {}).get(value)
-        if not dim:
-            return response.json({}, status=404)
-        return response.json(
-            {"key": key, "value": value, "customProperties": dim.get("customProperties"), "tags": dim.get("tags")}
-        )
-
-    @app.put("/v2/dimension/<key>/<value>")
-    async def put_dim(request, key, value):
-        content = request.json
-        dims[key][value] = content
-        return response.json({})
-
-    @app.patch("/v2/dimension/<key>/<value>/_/sfxagent")
-    async def patch_dim(request, key, value):
-        content = request.json
-
-        # The API won't accept these on this endpoint so make sure they aren't
-        # present
-        assert content.get("key") is None
-        assert content.get("value") is None
-
-        content["key"] = key
-        content["value"] = value
-
-        prop_keys_to_delete = []
-        props_to_add = content.get("customProperties", {})
-        for k, v in props_to_add.items():
-            if v is None:
-                prop_keys_to_delete.append(k)
-
-        for k in prop_keys_to_delete:
-            del props_to_add[k]
-
-        existing = dims[key].get(value)
-        if not existing:
-            dims[key][value] = {"customProperties": props_to_add, "tags": content.get("tags", [])}
-            return response.json({})
-
-        existing_props = existing.get("customProperties", {})
-        existing_props.update(props_to_add)
-        existing["customProperties"] = existing_props
-
-        for k in prop_keys_to_delete:
-            del existing_props[k]
-
-        existing_tags = existing.get("tags", [])
-        existing_tags.extend(content.get("tags", []))
-        existing["tags"] = existing_tags
-
-        for tag in content.get("tagsToRemove", []):
-            existing_tags.remove(tag)
-
-        return response.json(existing)
 
     return app
 
@@ -141,92 +55,37 @@ def _make_fake_api(dims):
 # The fake servers will be stopped once the context manager block is exited.
 # pylint: disable=too-many-locals,too-many-statements
 @contextmanager
-def start(ip_addr="127.0.0.1", ingest_port=0, api_port=0):
+def start(ip_addr="127.0.0.1", ingest_port=0):
     # Data structures are thread-safe due to the GIL
-    _dp_upload_queue = Queue()
     _datapoints = []
-    _datapoints_by_metric = defaultdict(list)
-    _datapoints_by_dim = defaultdict(list)
-    _events = []
-    _spans = []
-    _dims = defaultdict(defaultdict)
 
-    ingest_app = _make_fake_ingest(_dp_upload_queue, _events, _spans)
-    api_app = _make_fake_api(_dims)
+    ingest_app = _make_fake_ingest(_datapoints)
 
     [ingest_sock, _ingest_port] = bind_tcp_socket(ip_addr, ingest_port)
-    [api_sock, _api_port] = bind_tcp_socket(ip_addr, api_port)
 
     loop = asyncio.new_event_loop()
 
-    async def start_servers():
+    async def start_server():
         ingest_app.config.REQUEST_TIMEOUT = ingest_app.config.KEEP_ALIVE_TIMEOUT = 1000
-        api_app.config.REQUEST_TIMEOUT = api_app.config.KEEP_ALIVE_TIMEOUT = 1000
         ingest_server = ingest_app.create_server(sock=ingest_sock, access_log=False)
-        api_server = api_app.create_server(sock=api_sock, access_log=False)
 
         loop.create_task(ingest_server)
-        loop.create_task(api_server)
 
-    loop.create_task(start_servers())
+    loop.create_task(start_server())
     threading.Thread(target=loop.run_forever).start()
-
-    def _add_datapoints():
-        """
-        This is an attempt at making the datapoint endpoint have more throughput for heavy load tests.
-        """
-        while True:
-            dp_upload = _dp_upload_queue.get()
-            if dp_upload is STOP:
-                return
-            _datapoints.extend(dp_upload.datapoints)  # pylint: disable=no-member
-            for dp in dp_upload.datapoints:  # pylint: disable=no-member
-                _datapoints_by_metric[dp.metric].append(dp)
-                for dim in dp.dimensions:
-                    _datapoints_by_dim[f"{dim.key}:{dim.value}"].append(dp)
-
-    threading.Thread(target=_add_datapoints).start()
 
     class FakeBackend:  # pylint: disable=too-few-public-methods
         ingest_host = ip_addr
         ingest_port = _ingest_port
         ingest_url = f"http://{ingest_host}:{ingest_port}"
 
-        api_host = ip_addr
-        api_port = _api_port
-        api_url = f"http://{api_host}:{api_port}"
-
         datapoints = _datapoints
-        datapoints_by_metric = _datapoints_by_metric
-        datapoints_by_dim = _datapoints_by_dim
-        events = _events
-        spans = _spans
-        dims = _dims
-
-        def dump_json(self):
-            out = OrderedDict()
-            dps = [dp[0] for dp in self.datapoints_by_metric.values()]
-            metrics = {(dp.metric, dp.metricType) for dp in dps}
-            out["metrics"] = {metric: {"type": get_metric_type(metric_type)} for metric, metric_type in sorted(metrics)}
-            out["dimensions"] = sorted(set(self.datapoints_by_dim))
-            out["common_dimensions"] = []
-
-            # Set dimensions that are present on all datapoints.
-            for dim, dps in self.datapoints_by_dim.items():
-                if len({dp.metric for dp in dps}) == len(metrics):
-                    out["common_dimensions"].append(dim)
-
-            json.dump(out, sys.stdout, indent=2)
 
         def reset_datapoints(self):
             self.datapoints.clear()
-            self.datapoints_by_metric.clear()
-            self.datapoints_by_dim.clear()
 
     try:
         yield FakeBackend()
     finally:
         ingest_sock.close()
-        api_sock.close()
         loop.stop()
-        _dp_upload_queue.put(STOP)
--- tests/splunk_test.py
+++ tests/splunk_test.py
@@ -3,15 +3,13 @@ import time
 from functools import partial as p
 
 import pytest
-
 from tests.helpers import fake_backend
-from tests.helpers.assertions import container_cmd_exit_0, has_datapoint_with_metric_name, http_status
-from tests.helpers.formatting import print_dp_or_event
+from tests.helpers.assertions import has_datapoint, http_status
 from tests.helpers.util import container_ip, get_host_ip, run_service, wait_for
 from tests.paths import REPO_ROOT_DIR
 
+SPLUNK_VERSIONS = os.environ.get("SPLUNK_VERSIONS", "6.5.0,7.0.0,8.0.0").split(",")
 
-SPLUNK_VERSIONS = os.environ.get("SPLUNK_VERSIONS", "6.5.0,7.0.0,8.5.0").split(",")
 
 @pytest.mark.parametrize("splunk_version", SPLUNK_VERSIONS)
 def test_signalfx_forwarder_app(splunk_version):
@@ -25,32 +23,42 @@ def test_signalfx_forwarder_app(splunk_version):
         ) as cont:
             splunk_host = container_ip(cont)
             assert wait_for(p(http_status, url=f"http://{splunk_host}:8000", status=[200]), 120), "service didn't start"
+
             time.sleep(5)
+
             code, output = cont.exec_run(
                 "/code/splunk-forwarder/test-services/splunk/install-app.sh",
                 environment={"INGEST_HOST": backend.ingest_url},
                 user="splunk",
             )
-            print(output.decode("utf-8"))
-            assert code == 0, "Failed to install app!"
+
+            assert code == 0, f"Failed to install app: {output.decode('utf-8')}"
+
             assert wait_for(p(http_status, url=f"http://{splunk_host}:8000", status=[200]), 120), "service didn't start"
+
             try:
-                code, output = cont.exec_run("/code/splunk-forwarder/test-services/splunk/run-search.sh", user="splunk", environment={"SFX_CMD": "tosfx"})
-                print(output.decode("utf-8"))
-                assert code == 0, "tosfx query failed"
-                assert wait_for(p(has_datapoint_with_metric_name, backend, "eventCount"), timeout_seconds=60)
-                assert wait_for(p(has_datapoint_with_metric_name, backend, "kilobytes"), timeout_seconds=60)
-                code, output = cont.exec_run("/code/splunk-forwarder/test-services/splunk/run-search.sh", user="splunk", environment={"SFX_CMD": "streamtosfx"})
-                print(output.decode("utf-8"))
-                assert code == 0, "streamtosfx query failed"
+                code, output = cont.exec_run(
+                    "/code/splunk-forwarder/test-services/splunk/run-search.sh",
+                    user="splunk",
+                    environment={"SFX_CMD": "tosfx"},
+                )
+                assert code == 0, f"tosfx query failed: {output.decode('utf-8')}"
+
+                assert wait_for(p(has_datapoint, backend, metric="eventCount"), timeout_seconds=60)
+                assert wait_for(p(has_datapoint, backend, metric="kilobytes"), timeout_seconds=60)
+
                 backend.reset_datapoints()
-                assert wait_for(p(has_datapoint_with_metric_name, backend, "eventCount"), timeout_seconds=60)
-                assert wait_for(p(has_datapoint_with_metric_name, backend, "kilobytes"), timeout_seconds=60)
+
+                code, output = cont.exec_run(
+                    "/code/splunk-forwarder/test-services/splunk/run-search.sh",
+                    user="splunk",
+                    environment={"SFX_CMD": "streamtosfx"},
+                )
+                assert code == 0, f"streamtosfx query failed: {output.decode('utf-8')}"
+
+                assert wait_for(p(has_datapoint, backend, metric="eventCount"), timeout_seconds=60)
+                assert wait_for(p(has_datapoint, backend, metric="kilobytes"), timeout_seconds=60)
             finally:
                 print("\nDatapoints received:")
                 for dp in backend.datapoints:
-                    print_dp_or_event(dp)
-                print("\nEvents received:")
-                for event in backend.events:
-                    print_dp_or_event(event)
-                print(f"\nDimensions set: {backend.dims}")
+                    print(dp)
